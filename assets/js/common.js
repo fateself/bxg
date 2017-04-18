61d8a84e0d127e13fbca91c2685229b1faf594c5
binbin <651044782@qq.com>
2017-04-18 21:55:51 +0800

    登陆

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,8 +1,20 @@
-
+/*
 	NProgress.start();
 
 	NProgress.done();
 
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
-	});
+	});*/
+  console.log(1);
+  if ( !$.cookie('PHPSESSID') && location.pathname != "/index.php/login" ) {
+    console.log(1);
+    location.pathname = "/index.php/login";
+  }
+
+  // 页面加载的时候 从 cookie 中取出用户信息, 加载 图片与名字
+  var profile = JSON.parse( $.cookie( 'profile' ) );
+
+  // console.log( template );
+  var html = template( 'profileId', profile );
+  $( '.aside .profile' ).html( html );
